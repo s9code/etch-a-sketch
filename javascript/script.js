6faf7b7de38cf175b9f3219c991c8b537dea52a2
JaviG <86110696+s9code@users.noreply.github.com>
2026-07-03 19:04:48 +0200

    Generate 16x16 grid layout

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,6 @@ const divContainer = document.querySelector("#grid-container");
 
 for (let i = 0; i < 256; i++) {
     const gridCell = document.createElement("div");
-
+    gridCell.classList.add("gridCell");
     divContainer.appendChild(gridCell);
 };
