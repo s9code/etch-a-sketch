7a6a3b7f536f9f72e9daacb4bc17461847073318
JaviG <86110696+s9code@users.noreply.github.com>
2026-07-04 19:21:49 +0200

    Add dynamic grid resizing and input validation

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,56 @@
 const divContainer = document.querySelector("#grid-container");
+const userResize = document.querySelector("#resize-btn");
+const dltCells = document.querySelector("#dlt-cells");
 
-for (let i = 0; i < 256; i++) {
-    const gridCell = document.createElement("div");
-    gridCell.classList.add("gridCell");
-    divContainer.appendChild(gridCell);
 
-    gridCell.addEventListener("mouseenter", () => {
-        gridCell.classList.add("gridColor");
-    })
+userResize.addEventListener("click", () => {
+
+    const userInput = parseInt(prompt("¿Cuantos cuadrados quieres por lado?"));
+
+    if (userInput > 100 || userInput <= 0 || isNaN(userInput)) {
+
+        alert("Debes insertar un numero y no debe exceder de 100");
+        divContainer.innerHTML = "";
+
+    } else {
+
+        createGrid(userInput);
+
+    };
+
+});
+
+dltCells.addEventListener("click", () => {
+
+    divContainer.innerHTML = "";
+
+    createGrid(40);
+
+})
+
+function createGrid (gridSize) {
+
+    divContainer.innerHTML = "";
+
+    const totalCells = gridSize * gridSize;
+
+    const cellSize = 960 / gridSize;
+
+    for (let i = 0; i < totalCells; i++) {
+
+        const gridCell = document.createElement("div");
+
+        divContainer.appendChild(gridCell);
+
+        gridCell.addEventListener("mouseenter", () => {
+            gridCell.classList.add("gridColor");
+        });
+
+        gridCell.style.width = `${cellSize}px`;
+        gridCell.style.height = `${cellSize}px`;
+
+    };
+
 };
+
+createGrid(40);
